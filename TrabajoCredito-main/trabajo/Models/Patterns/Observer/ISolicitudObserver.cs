1c6da2c0ddd03e792fe73621fbc738b13d1faccd
RAFAEL_ROSALES <rosalespapuicorafa@gmail.com>
2026-08-24 21:33:34 -0500

    Delete TrabajoCredito-main directory

--- TrabajoCredito-main/trabajo/Models/Patterns/Observer/ISolicitudObserver.cs
+++
@@ -1,9 +0,0 @@
-﻿using trabajo.Models;
-
-namespace trabajo.Models.Patterns.Observer
-{
-    public interface ISolicitudObserver
-    {
-        void Actualizar(SolicitudCredito solicitud, string motivoCambio);
-    }
-}
